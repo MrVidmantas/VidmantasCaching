68e06b7fa989cd1ba06a8f44f5d7a0aa541b3a5a
Vidmantas Blazevicius <54100911+MrVidmantas@users.noreply.github.com>
2020-09-23 07:42:40 +0100

    Net Core Memory Provider DI (#14)
    Co-authored-by: Vidmantas Blazevicius <Vidmantas.Blazevicius@city-holdings.co.uk>

---
+++ Vidmantas.Caching.Provider.NetCoreMemory.Autofac/AutofacConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace Vidmantas.Caching.Provider.NetCoreMemory.Autofac
+{
+    #region Usings
+
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Runtime.Caching;
+    using global::Autofac;
+    using global::Autofac.Core;
+    using Interfaces;
+    using NetCoreMemory;
+
+    #endregion
+
+    public static class AutofacConfiguration
+    {
+        #region Public Methods
+
+        public static void UseNetMemoryProvider(this ContainerBuilder containerBuilder, int cacheExpiry = 5)
+        {
+            var cacheItemPolicy = new CacheItemPolicy
+            {
+                Priority = CacheItemPriority.Default,
+                SlidingExpiration = TimeSpan.FromMinutes(cacheExpiry),
+            };
+
+            containerBuilder.RegisterType<NetCoreMemoryCacheProvider>().As<ICacheProvider>().SingleInstance();
+            containerBuilder.RegisterType<MemoryCache>().As<ObjectCache>().SingleInstance()
+                .WithParameters(
+                    new List<Parameter> {
+                        new NamedParameter("name", "NetMemoryCache"),
+                        new NamedParameter("config", new NameValueCollection())
+                    });
+
+            containerBuilder.RegisterInstance(cacheItemPolicy).As<CacheItemPolicy>().SingleInstance();
+        }
+
+        #endregion
+    }
+}
